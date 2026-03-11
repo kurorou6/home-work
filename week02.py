0851bdbdd8909f5e5c4b5bc9aad1f0adbff1af13
kurorou6 <kurorou6@gmail.com>
2026-03-11 17:59:24 +0900

    Add files via upload

---
+++ week02.py
@@ -0,0 +1,10 @@
+# TODO 1: Variables
+student_name = "Alikhan"
+student_age = 19
+
+# TODO 2: Arithmetic
+next_year_age = student_age + 1
+
+# TODO 3: Print
+print("Hello, my name is", student_name)
+print("Next year, I will be", next_year_age)
